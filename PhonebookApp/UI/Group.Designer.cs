79d35a62aa4275906d0f1857ef12ba81cbffd110
kowsar11035 <kowsar11035@gmail.com>
2017-03-23 19:19:46 +0600

    logical changes

---
+++ PhonebookApp/UI/Group.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace PhonebookApp.UI
+{
+    partial class Group
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupNametextBox = new System.Windows.Forms.TextBox();
+            this.listView = new System.Windows.Forms.ListView();
+            this.groupNamelabel = new System.Windows.Forms.Label();
+            this.addbutton = new System.Windows.Forms.Button();
+            this.submitbutton = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10});
+            this.dataGridView.Location = new System.Drawing.Point(306, 91);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.Size = new System.Drawing.Size(849, 213);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "PersonId";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Visible = false;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Person Name";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Email";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Company Name";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Job Title";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Category";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Specialization";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Profession";
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Educational Level";
+            this.Column9.Name = "Column9";
+            this.Column9.ReadOnly = true;
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "Age Group Level";
+            this.Column10.Name = "Column10";
+            this.Column10.ReadOnly = true;
+            // 
+            // groupNametextBox
+            // 
+            this.groupNametextBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupNametextBox.Location = new System.Drawing.Point(32, 206);
+            this.groupNametextBox.Name = "groupNametextBox";
+            this.groupNametextBox.Size = new System.Drawing.Size(257, 26);
+            this.groupNametextBox.TabIndex = 3;
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9,
+            this.columnHeader10});
+            this.listView.GridLines = true;
+            this.listView.Location = new System.Drawing.Point(32, 328);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(1123, 147);
+            this.listView.TabIndex = 4;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // groupNamelabel
+            // 
+            this.groupNamelabel.AutoSize = true;
+            this.groupNamelabel.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupNamelabel.Location = new System.Drawing.Point(28, 169);
+            this.groupNamelabel.Name = "groupNamelabel";
+            this.groupNamelabel.Size = new System.Drawing.Size(116, 22);
+            this.groupNamelabel.TabIndex = 5;
+            this.groupNamelabel.Text = "Group Name";
+            // 
+            // addbutton
+            // 
+            this.addbutton.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addbutton.Location = new System.Drawing.Point(201, 91);
+            this.addbutton.Name = "addbutton";
+            this.addbutton.Size = new System.Drawing.Size(88, 49);
+            this.addbutton.TabIndex = 6;
+            this.addbutton.Text = "Add";
+            this.addbutton.UseVisualStyleBackColor = true;
+            this.addbutton.Click += new System.EventHandler(this.addbutton_Click);
+            // 
+            // submitbutton
+            // 
+            this.submitbutton.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitbutton.Location = new System.Drawing.Point(201, 255);
+            this.submitbutton.Name = "submitbutton";
+            this.submitbutton.Size = new System.Drawing.Size(88, 49);
+            this.submitbutton.TabIndex = 7;
+            this.submitbutton.Text = "Submit";
+            this.submitbutton.UseVisualStyleBackColor = true;
+            this.submitbutton.Click += new System.EventHandler(this.submitbutton_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Person Id";
+            this.columnHeader1.Width = 67;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Person Name";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Email";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Company Name";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Job Title";
+            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Category";
+            this.columnHeader6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Specialization";
+            this.columnHeader7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Profession";
+            this.columnHeader8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Educational Level";
+            this.columnHeader9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader10
+            // 
+            this.columnHeader10.Text = "Age Group Level";
+            this.columnHeader10.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Group
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1198, 579);
+            this.Controls.Add(this.submitbutton);
+            this.Controls.Add(this.addbutton);
+            this.Controls.Add(this.groupNamelabel);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.groupNametextBox);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Group";
+            this.Text = "Group";
+            this.Load += new System.EventHandler(this.Group_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.TextBox groupNametextBox;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.Label groupNamelabel;
+        private System.Windows.Forms.Button addbutton;
+        private System.Windows.Forms.Button submitbutton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.ColumnHeader columnHeader10;
+    }
+}
---
+++ PhonebookApp/UI/Group.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PhonebookApp.DbGateway;
+
+namespace PhonebookApp.UI
+{
+    public partial class Group : Form
+    {
+        private SqlConnection con;
+        private SqlCommand cmd;
+        private SqlDataReader rdr;
+        ConnectionString cs = new ConnectionString();
+        private SqlDataAdapter sda;
+        public int groupId, personid;
+        
+        public Group()
+        {
+            InitializeComponent();
+        }
+
+        private void FillPersonDetailsGrid()
+        {
+            try
+            {
+                con = new SqlConnection(cs.DBConn);
+                SqlDataAdapter sda = new SqlDataAdapter("SELECT Persons.PersonsId, Persons.PersonName, EmailBank.Email, Company.CompanyName, JobTitle.JobTitleName,Category.CategoryName, Specializations.Specialization, Profession.ProfessionName,EducationLevel.EducationLevelName,AgeGroup.AgeGroupLevel FROM Persons INNER JOIN EmailBank ON Persons.EmailBankId = EmailBank.EmailBankId INNER JOIN Category ON Persons.CategoryId = Category.CategoryId  INNER JOIN Company ON Persons.CompanyId = Company.CompanyId  INNER JOIN JobTitle ON Persons.JobTitleId = JobTitle.JobTitleId  INNER JOIN EducationLevel ON Persons.EducationLevelId = EducationLevel.EducationLevelId  INNER JOIN Profession ON Persons.ProfessionId = Profession.ProfessionId INNER JOIN Specializations ON Persons.SpecializationsId = Specializations.SpecializationsId INNER JOIN AgeGroup ON Persons.AgeGroupId=AgeGroup.AgeGroupId", con);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                dataGridView.Rows.Clear();
+                foreach (DataRow item in dt.Rows)
+                {
+                    int n = dataGridView.Rows.Add();
+                    dataGridView.Rows[n].Cells[0].Value = item[0].ToString();
+                    dataGridView.Rows[n].Cells[1].Value = item[1].ToString();
+                    dataGridView.Rows[n].Cells[2].Value = item[2].ToString();
+                    dataGridView.Rows[n].Cells[3].Value = item[3].ToString();
+                    dataGridView.Rows[n].Cells[4].Value = item[4].ToString();
+                    dataGridView.Rows[n].Cells[5].Value = item[5].ToString();
+                    dataGridView.Rows[n].Cells[6].Value = item[6].ToString();
+                    dataGridView.Rows[n].Cells[7].Value = item[7].ToString();
+                    dataGridView.Rows[n].Cells[8].Value = item[8].ToString();
+                    dataGridView.Rows[n].Cells[9].Value = item[9].ToString();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+        private void Group_Load(object sender, EventArgs e)
+        {
+            FillPersonDetailsGrid();
+        }
+
+        private void addbutton_Click(object sender, EventArgs e)
+        {
+
+
+            if (dataGridView.SelectedRows.Count > 0)
+            {                
+                try
+                {
+                    DataGridViewRow dr = dataGridView.SelectedRows[0];
+                    int personid = Convert.ToInt32(dataGridView.CurrentRow.Cells[0].Value.ToString());
+                    if (listView.Items.Count == 0)
+                    {
+                        ListViewItem lst = new ListViewItem();                        
+                        lst.Text=dr.Cells[0].Value.ToString();
+                        lst.SubItems.Add(dr.Cells[1].Value.ToString());
+                        lst.SubItems.Add(dr.Cells[2].Value.ToString());
+                        lst.SubItems.Add(dr.Cells[3].Value.ToString());
+                        lst.SubItems.Add(dr.Cells[4].Value.ToString());
+                        lst.SubItems.Add(dr.Cells[5].Value.ToString());
+                        lst.SubItems.Add(dr.Cells[6].Value.ToString());
+                        lst.SubItems.Add(dr.Cells[7].Value.ToString());
+                        lst.SubItems.Add(dr.Cells[8].Value.ToString());
+                        lst.SubItems.Add(dr.Cells[9].Value.ToString());
+                        listView.Items.Add(lst);
+                    }
+                    //String csVal = txtProductId.Text;
+
+                    else if (listView.FindItemWithText(personid.ToString()) == null)
+                    {
+                        ListViewItem lst1 = new ListViewItem();                       
+                        lst1.Text=dr.Cells[0].Value.ToString();
+                        lst1.SubItems.Add(dr.Cells[1].Value.ToString());
+                        lst1.SubItems.Add(dr.Cells[2].Value.ToString());
+                        lst1.SubItems.Add(dr.Cells[3].Value.ToString());
+                        lst1.SubItems.Add(dr.Cells[4].Value.ToString());
+                        lst1.SubItems.Add(dr.Cells[5].Value.ToString());
+                        lst1.SubItems.Add(dr.Cells[6].Value.ToString());
+                        lst1.SubItems.Add(dr.Cells[7].Value.ToString());
+                        lst1.SubItems.Add(dr.Cells[8].Value.ToString());
+                        lst1.SubItems.Add(dr.Cells[9].Value.ToString());
+                        listView.Items.Add(lst1);
+                    }
+                    else
+                    {
+                        MessageBox.Show("You Can Not Add Same Item More than one times", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("There is not any row selected, please select row and Click Add Button!");
+            }
+            
+        }
+
+        private void SaveGroup()
+        {
+            try
+            {
+                con = new SqlConnection(cs.DBConn);
+                con.Open();
+                string query = "insert into [Group] (GroupName) values (@d1)";
+                cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@d1", groupNametextBox.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                //MessageBox.Show("Submitted successfully", "Record", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void submitbutton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(groupNametextBox.Text))
+            {
+                MessageBox.Show("Please enter Group Name", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                SaveGroup();
+                try
+                { 
+                    con = new SqlConnection(cs.DBConn);
+                    con.Open();
+                    string query = "insert into [GroupMember] (GroupId,PersonsId) values (@d1,@d2)" + "SELECT CONVERT(int,SCOPE_IDENTITY())";
+                    cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@d1", groupId);
+                    cmd.Parameters.AddWithValue("@d2", personid);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("Submitted successfully", "Record", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
+

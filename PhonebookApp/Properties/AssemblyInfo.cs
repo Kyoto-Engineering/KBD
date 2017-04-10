d8462f4943f3adf8e74b46d4b665c12a92cb18f5
Shimul <shimul39ju@gmail.com>
2017-04-10 16:40:43 +0600

    Relesead first Version

--- PhonebookApp/Properties/AssemblyInfo.cs
+++ PhonebookApp/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PhonebookApp")]
+[assembly: AssemblyTitle("Kyoto Business Directory")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PhonebookApp")]
+[assembly: AssemblyCompany("Kyoto Engineering & Automation Ltd.")]
+[assembly: AssemblyProduct("Kyoto Business Directory")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Kyoto")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 

c54275642f3d5c3c56b50d55f3d9220f7e4b35aa
Ikramunnesa <ikraafi@gmail.com>
2017-05-04 10:22:31 +0600

    modify PhonebookApp/LogInUI/frmRegistration.Designer.cs

--- PhonebookApp/LogInUI/frmRegistration.Designer.cs
+++ PhonebookApp/LogInUI/frmRegistration.Designer.cs
@@ -63,11 +63,11 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(39, 188);
+            this.label8.Location = new System.Drawing.Point(9, 188);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(128, 22);
+            this.label8.Size = new System.Drawing.Size(183, 22);
             this.label8.TabIndex = 16;
-            this.label8.Text = "Email Address";
+            this.label8.Text = "Email Address          :";
             // 
             // contactNoTextBox
             // 
@@ -84,11 +84,11 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(68, 310);
+            this.label7.Location = new System.Drawing.Point(9, 310);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(99, 22);
+            this.label7.Size = new System.Drawing.Size(184, 22);
             this.label7.TabIndex = 14;
-            this.label7.Text = "ContactNo";
+            this.label7.Text = "Contact No               :";
             // 
             // departmentTextBox
             // 
@@ -103,11 +103,11 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(68, 263);
+            this.label6.Location = new System.Drawing.Point(9, 265);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(107, 22);
+            this.label6.Size = new System.Drawing.Size(182, 22);
             this.label6.TabIndex = 12;
-            this.label6.Text = "Department";
+            this.label6.Text = "Department              :";
             // 
             // designationTextBox
             // 
@@ -122,11 +122,11 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(68, 226);
+            this.label5.Location = new System.Drawing.Point(9, 226);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(107, 22);
+            this.label5.Size = new System.Drawing.Size(182, 22);
             this.label5.TabIndex = 10;
-            this.label5.Text = "Designation";
+            this.label5.Text = "Designation              :";
             // 
             // userNameTextBox
             // 
@@ -186,41 +186,41 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(81, 145);
+            this.label4.Location = new System.Drawing.Point(9, 147);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(95, 22);
+            this.label4.Size = new System.Drawing.Size(185, 22);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Full Name";
+            this.label4.Text = "Full Name                 :";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(83, 106);
+            this.label3.Location = new System.Drawing.Point(9, 106);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(93, 22);
+            this.label3.Size = new System.Drawing.Size(183, 22);
             this.label3.TabIndex = 2;
-            this.label3.Text = "User Type";
+            this.label3.Text = "User Type                 :";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(85, 67);
+            this.label2.Location = new System.Drawing.Point(9, 69);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(90, 22);
+            this.label2.Size = new System.Drawing.Size(185, 22);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Password";
+            this.label2.Text = "Password                  :";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(78, 30);
+            this.label1.Location = new System.Drawing.Point(9, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 22);
+            this.label1.Size = new System.Drawing.Size(181, 22);
             this.label1.TabIndex = 0;
-            this.label1.Text = "User Name";
+            this.label1.Text = "User Name               :";
             // 
             // groupBox1
             // 

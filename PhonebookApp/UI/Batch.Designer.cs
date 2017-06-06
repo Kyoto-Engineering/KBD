11ead1181c0a2dd0f8a349f5a964848377cec0ac
kowsar11035 <kowsar.jony11035@gmail.com>
2017-06-06 16:13:48 +0600

    add Batch UI

---
+++ PhonebookApp/UI/Batch.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace PhonebookApp.UI
+{
+    partial class Batch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.PersonIdtextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.importOrderDate = new System.Windows.Forms.DateTimePicker();
+            this.label14 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.submitButton);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.PersonIdtextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.importOrderDate);
+            this.groupBox1.Controls.Add(this.label14);
+            this.groupBox1.Location = new System.Drawing.Point(12, 70);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(459, 277);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // submitButton
+            // 
+            this.submitButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.submitButton.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitButton.ForeColor = System.Drawing.Color.Fuchsia;
+            this.submitButton.Location = new System.Drawing.Point(317, 199);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(111, 55);
+            this.submitButton.TabIndex = 43;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = false;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Yellow;
+            this.button1.Location = new System.Drawing.Point(317, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 63);
+            this.button1.TabIndex = 42;
+            this.button1.Text = "Add To Chart    ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PersonIdtextBox
+            // 
+            this.PersonIdtextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PersonIdtextBox.Location = new System.Drawing.Point(113, 156);
+            this.PersonIdtextBox.Name = "PersonIdtextBox";
+            this.PersonIdtextBox.Size = new System.Drawing.Size(150, 26);
+            this.PersonIdtextBox.TabIndex = 41;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(6, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 22);
+            this.label2.TabIndex = 40;
+            this.label2.Text = "Person ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(56, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 22);
+            this.label1.TabIndex = 38;
+            this.label1.Text = "POD";
+            // 
+            // importOrderDate
+            // 
+            this.importOrderDate.Enabled = false;
+            this.importOrderDate.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.importOrderDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.importOrderDate.Location = new System.Drawing.Point(113, 33);
+            this.importOrderDate.Name = "importOrderDate";
+            this.importOrderDate.Size = new System.Drawing.Size(150, 32);
+            this.importOrderDate.TabIndex = 36;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.ForeColor = System.Drawing.Color.Black;
+            this.label14.Location = new System.Drawing.Point(57, 39);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(54, 22);
+            this.label14.TabIndex = 37;
+            this.label14.Text = "Date ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listView1);
+            this.groupBox2.Location = new System.Drawing.Point(512, 64);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(199, 362);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader3});
+            this.listView1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(34, 37);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(139, 288);
+            this.listView1.TabIndex = 18;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "columnHeader";
+            this.columnHeader1.Width = 11;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Person Id";
+            this.columnHeader3.Width = 120;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(130, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 44;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Batch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1096, 438);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Batch";
+            this.Text = "Batch";
+            this.Load += new System.EventHandler(this.Batch_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        public System.Windows.Forms.DateTimePicker importOrderDate;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox PersonIdtextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView listView1;
+        internal System.Windows.Forms.ColumnHeader columnHeader1;
+        internal System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ PhonebookApp/UI/Batch.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PhonebookApp.DbGateway;
+using PhonebookApp.LogInUI;
+
+namespace PhonebookApp.UI
+{
+    public partial class Batch : Form
+    {
+        SqlConnection _con;
+        SqlCommand _cmd;
+        ConnectionString _cs = new ConnectionString();
+        SqlDataReader rdr;
+        public string user_id;
+        public int Batchid;
+        public Batch()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+     
+
+            if (PersonIdtextBox.Text == "")
+            {
+                MessageBox.Show("You must Enter Person Id", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                PersonIdtextBox.Focus();
+                return;
+            }
+
+            try
+            {
+                if (listView1.Items.Count == 0)
+                {
+                    ListViewItem lst = new ListViewItem();
+                    lst.SubItems.Add(PersonIdtextBox.Text);
+                   
+                    listView1.Items.Add(lst);
+                    PersonIdtextBox.Text = "";                  
+                    return;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            if (listView1.Items.Count == 0)
+            {
+                MessageBox.Show("Please add to Chart first", "Report", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                PersonIdtextBox.Focus();
+                return;
+            }
+
+            _con = new SqlConnection(_cs.DBConn);
+            string cd1 = "INSERT INTO Batch (POD,UserId,BatchTime) VALUES (@d1,@d2,@d3)" + "SELECT CONVERT(int, SCOPE_IDENTITY())";
+            _cmd = new SqlCommand(cd1, _con);
+            //_cmd.Parameters.Add(new SqlParameter("@d1",
+            //string.IsNullOrEmpty(PodtextBox.Text) ? (object)DBNull.Value : PodtextBox.Text));
+            _cmd.Parameters.AddWithValue("@d2", user_id);
+            _cmd.Parameters.AddWithValue("@d3", DateTime.UtcNow.ToLocalTime());
+            _con.Open();
+            Batchid = (int)_cmd.ExecuteScalar();
+            _con.Close();
+            try
+            {
+                for (int i = 0; i <= listView1.Items.Count - 1; i++)
+                {
+                    _con = new SqlConnection(_cs.DBConn);
+                    string cd = "INSERT INTO DetailsOfBatch (BatchID,PersonsId) VALUES (@d1,@d2)";
+                    _cmd = new SqlCommand(cd, _con);
+                    _cmd.Parameters.AddWithValue("@d1", PersonIdtextBox);
+                    _cmd.Parameters.AddWithValue("d2", listView1.Items[i].SubItems[1].Text);
+                    _con.Open();
+                    _cmd.ExecuteNonQuery();
+                    _con.Close();
+
+                }              
+                MessageBox.Show("Successfully Submitted.", "Record", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                listView1.Items.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Batch_Load(object sender, EventArgs e)
+        {
+            user_id = frmLogin.uId.ToString();
+        }
+    }
+}

356e32785ac14b35d6c04033cf34aab2f1edcd06
kowsar11035 <kowsar.jony11035@gmail.com>
2017-06-18 20:35:33 +0600

    add png in Main UI

--- PhonebookApp/UI/MainUI.Designer.cs
+++ PhonebookApp/UI/MainUI.Designer.cs
@@ -161,7 +161,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImage = global::PhonebookApp.Properties.Resources.BusinessDirectory;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(1038, 641);
             this.ControlBox = false;

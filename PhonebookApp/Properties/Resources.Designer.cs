a5d4d67b5e1d5df52bb9d120b14a53b38de3ab8e
Ikramunnesa <ikraafi@gmail.com>
2017-08-14 12:44:46 +0600

    kbd plus new version released to share with OR code

--- PhonebookApp/Properties/Resources.Designer.cs
+++ PhonebookApp/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace PhonebookApp.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Business_Directory_Plus {
+            get {
+                object obj = ResourceManager.GetObject("Business Directory Plus", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap BusinessDirectory {
             get {
                 object obj = ResourceManager.GetObject("BusinessDirectory", resourceCulture);

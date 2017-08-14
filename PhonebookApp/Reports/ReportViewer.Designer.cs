249014d5de200b3d53ff9d4e19db5a10d7b8b866
Ikramunnesa <ikraafi@gmail.com>
2017-08-14 12:32:07 +0600

    design modified

--- PhonebookApp/Reports/ReportViewer.Designer.cs
+++ PhonebookApp/Reports/ReportViewer.Designer.cs
@@ -47,6 +47,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(797, 470);
             this.Controls.Add(this.crystalReportViewer1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));

d27cb69ecf37e3a98577925dc010c5b5e28e0e2c
Saqib-Tanmoy <saqib.tanmoy@gmail.com>
2017-11-16 18:10:41 +0600

    modified

--- PhonebookApp/UI/frmNewEntry.Designer.cs
+++ PhonebookApp/UI/frmNewEntry.Designer.cs
@@ -35,18 +35,22 @@
             this.button4 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.SearchCompanybutton);
             this.groupBox1.Controls.Add(this.SearchContactbutton);
             this.groupBox1.Controls.Add(this.button4);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Location = new System.Drawing.Point(116, 84);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(375, 220);
+            this.groupBox1.Size = new System.Drawing.Size(546, 220);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
@@ -68,7 +72,7 @@
             this.SearchContactbutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.SearchContactbutton.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SearchContactbutton.ForeColor = System.Drawing.Color.Yellow;
-            this.SearchContactbutton.Location = new System.Drawing.Point(221, 131);
+            this.SearchContactbutton.Location = new System.Drawing.Point(208, 131);
             this.SearchContactbutton.Name = "SearchContactbutton";
             this.SearchContactbutton.Size = new System.Drawing.Size(121, 69);
             this.SearchContactbutton.TabIndex = 39;
@@ -94,7 +98,7 @@
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.Blue;
-            this.button1.Location = new System.Drawing.Point(221, 36);
+            this.button1.Location = new System.Drawing.Point(207, 36);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(121, 69);
             this.button1.TabIndex = 37;
@@ -107,18 +111,43 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Lime;
-            this.label1.Location = new System.Drawing.Point(240, 27);
+            this.label1.Location = new System.Drawing.Point(317, 29);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(142, 33);
             this.label1.TabIndex = 1;
             this.label1.Text = "New Entry";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Blue;
+            this.button2.Location = new System.Drawing.Point(380, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 69);
+            this.button2.TabIndex = 41;
+            this.button2.Text = "New Foreign Contact";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Yellow;
+            this.button3.Location = new System.Drawing.Point(380, 131);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 69);
+            this.button3.TabIndex = 42;
+            this.button3.Text = "Search Foreign Contact";
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // frmNewEntry
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SeaGreen;
-            this.ClientSize = new System.Drawing.Size(651, 408);
+            this.ClientSize = new System.Drawing.Size(760, 408);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -139,5 +168,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button SearchCompanybutton;
         private System.Windows.Forms.Button SearchContactbutton;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
     }
 }

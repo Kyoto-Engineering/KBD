70493c5d6aa411473413df30920a6815d74b488d
Fariha Hossain <farihafiha@gmail.com>
2017-12-09 13:50:01 +0600

    frmViewAndReport.Designer.cs

--- PhonebookApp/UI/frmViewAndReport.Designer.cs
+++ PhonebookApp/UI/frmViewAndReport.Designer.cs
@@ -44,18 +44,18 @@
             this.groupBox1.Controls.Add(this.ViewCompanybutton);
             this.groupBox1.Controls.Add(this.reportButton);
             this.groupBox1.Controls.Add(this.personDetailsButton);
-            this.groupBox1.Location = new System.Drawing.Point(142, 115);
+            this.groupBox1.Location = new System.Drawing.Point(142, 68);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(423, 269);
+            this.groupBox1.Size = new System.Drawing.Size(492, 245);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
             // PersonUnderACompanyButton
             // 
-            this.PersonUnderACompanyButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.PersonUnderACompanyButton.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PersonUnderACompanyButton.ForeColor = System.Drawing.Color.Blue;
-            this.PersonUnderACompanyButton.Location = new System.Drawing.Point(231, 35);
+            this.PersonUnderACompanyButton.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.PersonUnderACompanyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PersonUnderACompanyButton.ForeColor = System.Drawing.Color.Black;
+            this.PersonUnderACompanyButton.Location = new System.Drawing.Point(289, 35);
             this.PersonUnderACompanyButton.Name = "PersonUnderACompanyButton";
             this.PersonUnderACompanyButton.Size = new System.Drawing.Size(134, 70);
             this.PersonUnderACompanyButton.TabIndex = 51;
@@ -65,10 +65,10 @@
             // 
             // ViewCompanybutton
             // 
-            this.ViewCompanybutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.ViewCompanybutton.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ViewCompanybutton.ForeColor = System.Drawing.Color.Blue;
-            this.ViewCompanybutton.Location = new System.Drawing.Point(33, 144);
+            this.ViewCompanybutton.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ViewCompanybutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ViewCompanybutton.ForeColor = System.Drawing.Color.Black;
+            this.ViewCompanybutton.Location = new System.Drawing.Point(53, 144);
             this.ViewCompanybutton.Name = "ViewCompanybutton";
             this.ViewCompanybutton.Size = new System.Drawing.Size(134, 70);
             this.ViewCompanybutton.TabIndex = 50;
@@ -78,23 +78,23 @@
             // 
             // reportButton
             // 
-            this.reportButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.reportButton.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.reportButton.ForeColor = System.Drawing.Color.Blue;
-            this.reportButton.Location = new System.Drawing.Point(231, 144);
+            this.reportButton.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.reportButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reportButton.ForeColor = System.Drawing.Color.Black;
+            this.reportButton.Location = new System.Drawing.Point(289, 144);
             this.reportButton.Name = "reportButton";
             this.reportButton.Size = new System.Drawing.Size(134, 70);
             this.reportButton.TabIndex = 49;
-            this.reportButton.Text = "Report";
+            this.reportButton.Text = "Print";
             this.reportButton.UseVisualStyleBackColor = false;
             this.reportButton.Click += new System.EventHandler(this.reportButton_Click);
             // 
             // personDetailsButton
             // 
-            this.personDetailsButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.personDetailsButton.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.personDetailsButton.ForeColor = System.Drawing.Color.Blue;
-            this.personDetailsButton.Location = new System.Drawing.Point(33, 35);
+            this.personDetailsButton.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.personDetailsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.personDetailsButton.ForeColor = System.Drawing.Color.Black;
+            this.personDetailsButton.Location = new System.Drawing.Point(53, 35);
             this.personDetailsButton.Name = "personDetailsButton";
             this.personDetailsButton.Size = new System.Drawing.Size(134, 70);
             this.personDetailsButton.TabIndex = 48;
@@ -105,20 +105,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(232, 26);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(306, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(215, 33);
+            this.label1.Size = new System.Drawing.Size(183, 29);
             this.label1.TabIndex = 1;
-            this.label1.Text = "View And Report";
+            this.label1.Text = "View And Print";
             // 
             // frmViewAndReport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.ClientSize = new System.Drawing.Size(687, 438);
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(793, 364);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));

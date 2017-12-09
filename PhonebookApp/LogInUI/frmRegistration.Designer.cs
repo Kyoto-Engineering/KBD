b39f5c4208bde9014398a826d587fdbff33d66f5
Fariha Hossain <farihafiha@gmail.com>
2017-12-09 13:49:09 +0600

    frmRegistration.Designer.cs

--- PhonebookApp/LogInUI/frmRegistration.Designer.cs
+++ PhonebookApp/LogInUI/frmRegistration.Designer.cs
@@ -64,10 +64,10 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.Location = new System.Drawing.Point(9, 188);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(183, 22);
+            this.label8.Size = new System.Drawing.Size(179, 20);
             this.label8.TabIndex = 16;
             this.label8.Text = "Email Address          :";
             // 
@@ -85,10 +85,10 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(9, 310);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(184, 22);
+            this.label7.Size = new System.Drawing.Size(179, 20);
             this.label7.TabIndex = 14;
             this.label7.Text = "Contact No               :";
             // 
@@ -105,10 +105,10 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(9, 265);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(182, 22);
+            this.label6.Size = new System.Drawing.Size(179, 20);
             this.label6.TabIndex = 12;
             this.label6.Text = "Department              :";
             // 
@@ -125,10 +125,10 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(9, 226);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(182, 22);
+            this.label5.Size = new System.Drawing.Size(180, 20);
             this.label5.TabIndex = 10;
             this.label5.Text = "Designation              :";
             // 
@@ -178,12 +178,12 @@
             // 
             // userButton
             // 
-            this.userButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.userButton.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.userButton.ForeColor = System.Drawing.Color.Blue;
+            this.userButton.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.userButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userButton.ForeColor = System.Drawing.Color.Black;
             this.userButton.Location = new System.Drawing.Point(291, 363);
             this.userButton.Name = "userButton";
-            this.userButton.Size = new System.Drawing.Size(124, 49);
+            this.userButton.Size = new System.Drawing.Size(124, 34);
             this.userButton.TabIndex = 9;
             this.userButton.Text = "Create ";
             this.userButton.UseVisualStyleBackColor = false;
@@ -192,40 +192,40 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(9, 147);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(185, 22);
+            this.label4.Size = new System.Drawing.Size(179, 20);
             this.label4.TabIndex = 3;
             this.label4.Text = "Full Name                 :";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(9, 106);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(183, 22);
+            this.label3.Size = new System.Drawing.Size(180, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "User Type                 :";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(9, 69);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(185, 22);
+            this.label2.Size = new System.Drawing.Size(181, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Password                  :";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(9, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(181, 22);
+            this.label1.Size = new System.Drawing.Size(178, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "User Name               :";
             // 
@@ -248,6 +248,7 @@
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(41, 7);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(528, 436);
@@ -258,7 +259,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Olive;
+            this.BackColor = System.Drawing.Color.Silver;
             this.ClientSize = new System.Drawing.Size(607, 463);
             this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));

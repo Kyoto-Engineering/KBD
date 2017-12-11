7f833dd498d8a51d51a02f0be55dedb388f77e00
Fariha Hossain <farihafiha@gmail.com>
2017-12-11 13:54:05 +0600

    ManageGroup.Designer.cs

--- PhonebookApp/UI/ManageGroup.Designer.cs
+++ PhonebookApp/UI/ManageGroup.Designer.cs
@@ -31,7 +31,6 @@
             this.ManageGroupsbutton = new System.Windows.Forms.Button();
             this.MemberAddedToGroupbutton = new System.Windows.Forms.Button();
             this.NewGroupCreationbutton = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ManageGroupsbutton
@@ -39,9 +38,9 @@
             this.ManageGroupsbutton.BackColor = System.Drawing.SystemColors.Control;
             this.ManageGroupsbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ManageGroupsbutton.ForeColor = System.Drawing.Color.Black;
-            this.ManageGroupsbutton.Location = new System.Drawing.Point(345, 206);
+            this.ManageGroupsbutton.Location = new System.Drawing.Point(70, 219);
             this.ManageGroupsbutton.Name = "ManageGroupsbutton";
-            this.ManageGroupsbutton.Size = new System.Drawing.Size(205, 73);
+            this.ManageGroupsbutton.Size = new System.Drawing.Size(196, 59);
             this.ManageGroupsbutton.TabIndex = 49;
             this.ManageGroupsbutton.Text = "Remove Member from Group";
             this.ManageGroupsbutton.UseVisualStyleBackColor = false;
@@ -52,9 +51,9 @@
             this.MemberAddedToGroupbutton.BackColor = System.Drawing.SystemColors.Control;
             this.MemberAddedToGroupbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MemberAddedToGroupbutton.ForeColor = System.Drawing.Color.Black;
-            this.MemberAddedToGroupbutton.Location = new System.Drawing.Point(70, 206);
+            this.MemberAddedToGroupbutton.Location = new System.Drawing.Point(70, 137);
             this.MemberAddedToGroupbutton.Name = "MemberAddedToGroupbutton";
-            this.MemberAddedToGroupbutton.Size = new System.Drawing.Size(196, 73);
+            this.MemberAddedToGroupbutton.Size = new System.Drawing.Size(196, 54);
             this.MemberAddedToGroupbutton.TabIndex = 48;
             this.MemberAddedToGroupbutton.Text = "Add Member to Group";
             this.MemberAddedToGroupbutton.UseVisualStyleBackColor = false;
@@ -73,25 +72,12 @@
             this.NewGroupCreationbutton.UseVisualStyleBackColor = false;
             this.NewGroupCreationbutton.Click += new System.EventHandler(this.NewGroupCreationbutton_Click);
             // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.SystemColors.Control;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(345, 63);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(205, 58);
-            this.button1.TabIndex = 51;
-            this.button1.Text = "Remove Group";
-            this.button1.UseVisualStyleBackColor = false;
-            // 
             // ManageGroup
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkGray;
-            this.ClientSize = new System.Drawing.Size(608, 367);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(402, 355);
             this.Controls.Add(this.NewGroupCreationbutton);
             this.Controls.Add(this.ManageGroupsbutton);
             this.Controls.Add(this.MemberAddedToGroupbutton);
@@ -107,6 +93,5 @@
         private System.Windows.Forms.Button ManageGroupsbutton;
         private System.Windows.Forms.Button MemberAddedToGroupbutton;
         private System.Windows.Forms.Button NewGroupCreationbutton;
-        private System.Windows.Forms.Button button1;
     }
 }

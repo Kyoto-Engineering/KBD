5befd5b281fa6299e83f8b31344c00bcb934194b
Saqib-Tanmoy <saqib.tanmoy@gmail.com>
2017-12-18 14:59:10 +0600

    batch report modified

--- PhonebookApp/Reports/ReportByBatch.Designer.cs
+++ PhonebookApp/Reports/ReportByBatch.Designer.cs
@@ -32,15 +32,16 @@
             this.getButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.batchIdCombobox = new System.Windows.Forms.ComboBox();
-            this.PortraitRadioButton = new System.Windows.Forms.RadioButton();
+            this.BatchReportforSuccessfulLetter = new System.Windows.Forms.RadioButton();
             this.LandscapeRadioButton = new System.Windows.Forms.RadioButton();
+            this.missingradioButton = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // getButton
             // 
             this.getButton.BackColor = System.Drawing.Color.WhiteSmoke;
             this.getButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.getButton.Location = new System.Drawing.Point(211, 186);
+            this.getButton.Location = new System.Drawing.Point(236, 178);
             this.getButton.Name = "getButton";
             this.getButton.Size = new System.Drawing.Size(74, 23);
             this.getButton.TabIndex = 0;
@@ -63,41 +64,55 @@
             this.batchIdCombobox.FormattingEnabled = true;
             this.batchIdCombobox.Location = new System.Drawing.Point(211, 22);
             this.batchIdCombobox.Name = "batchIdCombobox";
-            this.batchIdCombobox.Size = new System.Drawing.Size(121, 21);
+            this.batchIdCombobox.Size = new System.Drawing.Size(175, 21);
             this.batchIdCombobox.TabIndex = 2;
             this.batchIdCombobox.SelectedIndexChanged += new System.EventHandler(this.batchIdCombobox_SelectedIndexChanged);
             // 
-            // PortraitRadioButton
+            // BatchReportforSuccessfulLetter
             // 
-            this.PortraitRadioButton.AutoSize = true;
-            this.PortraitRadioButton.Location = new System.Drawing.Point(198, 60);
-            this.PortraitRadioButton.Name = "PortraitRadioButton";
-            this.PortraitRadioButton.Size = new System.Drawing.Size(139, 17);
-            this.PortraitRadioButton.TabIndex = 3;
-            this.PortraitRadioButton.TabStop = true;
-            this.PortraitRadioButton.Text = "Batch Report for Portrait";
-            this.PortraitRadioButton.UseVisualStyleBackColor = true;
-            this.PortraitRadioButton.CheckedChanged += new System.EventHandler(this.PortraitRadioButton_CheckedChanged);
+            this.BatchReportforSuccessfulLetter.AutoSize = true;
+            this.BatchReportforSuccessfulLetter.Location = new System.Drawing.Point(198, 60);
+            this.BatchReportforSuccessfulLetter.Name = "BatchReportforSuccessfulLetter";
+            this.BatchReportforSuccessfulLetter.Size = new System.Drawing.Size(188, 17);
+            this.BatchReportforSuccessfulLetter.TabIndex = 3;
+            this.BatchReportforSuccessfulLetter.TabStop = true;
+            this.BatchReportforSuccessfulLetter.Text = "Batch Report for Successful Letter";
+            this.BatchReportforSuccessfulLetter.UseVisualStyleBackColor = true;
+            this.BatchReportforSuccessfulLetter.CheckedChanged += new System.EventHandler(this.PortraitRadioButton_CheckedChanged);
             // 
             // LandscapeRadioButton
             // 
             this.LandscapeRadioButton.AutoSize = true;
             this.LandscapeRadioButton.Location = new System.Drawing.Point(198, 93);
             this.LandscapeRadioButton.Name = "LandscapeRadioButton";
-            this.LandscapeRadioButton.Size = new System.Drawing.Size(159, 17);
+            this.LandscapeRadioButton.Size = new System.Drawing.Size(200, 17);
             this.LandscapeRadioButton.TabIndex = 4;
             this.LandscapeRadioButton.TabStop = true;
-            this.LandscapeRadioButton.Text = "Batch Report for Landscape";
+            this.LandscapeRadioButton.Text = "Batch Report for Unsuccessful Letter";
             this.LandscapeRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // missingradioButton
+            // 
+            this.missingradioButton.AutoSize = true;
+            this.missingradioButton.Location = new System.Drawing.Point(198, 128);
+            this.missingradioButton.Name = "missingradioButton";
+            this.missingradioButton.Size = new System.Drawing.Size(171, 17);
+            this.missingradioButton.TabIndex = 5;
+            this.missingradioButton.TabStop = true;
+            this.missingradioButton.Text = "Batch Report for Missing Letter";
+            this.missingradioButton.UseVisualStyleBackColor = true;
+            this.missingradioButton.CheckedChanged += new System.EventHandler(this.missingradioButton_CheckedChanged);
+            this.missingradioButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.missingradioButton_MouseClick);
             // 
             // ReportByBatch
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(503, 284);
+            this.ClientSize = new System.Drawing.Size(544, 284);
+            this.Controls.Add(this.missingradioButton);
             this.Controls.Add(this.LandscapeRadioButton);
-            this.Controls.Add(this.PortraitRadioButton);
+            this.Controls.Add(this.BatchReportforSuccessfulLetter);
             this.Controls.Add(this.batchIdCombobox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.getButton);
@@ -105,6 +120,7 @@
             this.Name = "ReportByBatch";
             this.Text = "ReportByBatch";
             this.Load += new System.EventHandler(this.ReportByBatch_Load);
+            this.Click += new System.EventHandler(this.ReportByBatch_Click);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -115,7 +131,8 @@
         private System.Windows.Forms.Button getButton;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox batchIdCombobox;
-        private System.Windows.Forms.RadioButton PortraitRadioButton;
+        private System.Windows.Forms.RadioButton BatchReportforSuccessfulLetter;
         private System.Windows.Forms.RadioButton LandscapeRadioButton;
+        private System.Windows.Forms.RadioButton missingradioButton;
     }
 }

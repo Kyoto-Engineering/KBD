0a89ef36d6a8655900cb68191142409721163bcf
Fariha Hossain <farihafiha@gmail.com>
2017-12-19 16:54:14 +0600

    BatchUi changed

--- PhonebookApp/Reports/ReportByBatch.Designer.cs
+++ PhonebookApp/Reports/ReportByBatch.Designer.cs
@@ -35,84 +35,129 @@
             this.BatchReportforSuccessfulLetter = new System.Windows.Forms.RadioButton();
             this.LandscapeRadioButton = new System.Windows.Forms.RadioButton();
             this.missingradioButton = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // getButton
             // 
             this.getButton.BackColor = System.Drawing.Color.WhiteSmoke;
             this.getButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.getButton.Location = new System.Drawing.Point(236, 197);
+            this.getButton.Location = new System.Drawing.Point(184, 264);
             this.getButton.Name = "getButton";
-            this.getButton.Size = new System.Drawing.Size(74, 23);
+            this.getButton.Size = new System.Drawing.Size(11, 23);
             this.getButton.TabIndex = 0;
             this.getButton.Text = "GET";
             this.getButton.UseVisualStyleBackColor = false;
+            this.getButton.Visible = false;
             this.getButton.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(106, 42);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(272, 242);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 18);
+            this.label1.Size = new System.Drawing.Size(71, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Batch Id   :";
+            this.label1.Visible = false;
             // 
             // batchIdCombobox
             // 
             this.batchIdCombobox.FormattingEnabled = true;
-            this.batchIdCombobox.Location = new System.Drawing.Point(192, 41);
+            this.batchIdCombobox.Location = new System.Drawing.Point(496, 239);
             this.batchIdCombobox.Name = "batchIdCombobox";
-            this.batchIdCombobox.Size = new System.Drawing.Size(194, 21);
+            this.batchIdCombobox.Size = new System.Drawing.Size(17, 21);
             this.batchIdCombobox.TabIndex = 2;
+            this.batchIdCombobox.Visible = false;
             this.batchIdCombobox.SelectedIndexChanged += new System.EventHandler(this.batchIdCombobox_SelectedIndexChanged);
             // 
             // BatchReportforSuccessfulLetter
             // 
             this.BatchReportforSuccessfulLetter.AutoSize = true;
-            this.BatchReportforSuccessfulLetter.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BatchReportforSuccessfulLetter.Location = new System.Drawing.Point(173, 83);
+            this.BatchReportforSuccessfulLetter.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BatchReportforSuccessfulLetter.Location = new System.Drawing.Point(407, 277);
             this.BatchReportforSuccessfulLetter.Name = "BatchReportforSuccessfulLetter";
-            this.BatchReportforSuccessfulLetter.Size = new System.Drawing.Size(226, 20);
+            this.BatchReportforSuccessfulLetter.Size = new System.Drawing.Size(188, 17);
             this.BatchReportforSuccessfulLetter.TabIndex = 3;
             this.BatchReportforSuccessfulLetter.TabStop = true;
             this.BatchReportforSuccessfulLetter.Text = "Batch Report for Successful Letter";
             this.BatchReportforSuccessfulLetter.UseVisualStyleBackColor = true;
+            this.BatchReportforSuccessfulLetter.Visible = false;
             this.BatchReportforSuccessfulLetter.CheckedChanged += new System.EventHandler(this.PortraitRadioButton_CheckedChanged);
             // 
             // LandscapeRadioButton
             // 
             this.LandscapeRadioButton.AutoSize = true;
-            this.LandscapeRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LandscapeRadioButton.Location = new System.Drawing.Point(173, 116);
+            this.LandscapeRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LandscapeRadioButton.Location = new System.Drawing.Point(201, 274);
             this.LandscapeRadioButton.Name = "LandscapeRadioButton";
-            this.LandscapeRadioButton.Size = new System.Drawing.Size(241, 20);
+            this.LandscapeRadioButton.Size = new System.Drawing.Size(200, 17);
             this.LandscapeRadioButton.TabIndex = 4;
             this.LandscapeRadioButton.TabStop = true;
             this.LandscapeRadioButton.Text = "Batch Report for Unsuccessful Letter";
             this.LandscapeRadioButton.UseVisualStyleBackColor = true;
+            this.LandscapeRadioButton.Visible = false;
             // 
             // missingradioButton
             // 
             this.missingradioButton.AutoSize = true;
-            this.missingradioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.missingradioButton.Location = new System.Drawing.Point(173, 151);
+            this.missingradioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.missingradioButton.Location = new System.Drawing.Point(12, 271);
             this.missingradioButton.Name = "missingradioButton";
-            this.missingradioButton.Size = new System.Drawing.Size(207, 20);
+            this.missingradioButton.Size = new System.Drawing.Size(171, 17);
             this.missingradioButton.TabIndex = 5;
             this.missingradioButton.TabStop = true;
             this.missingradioButton.Text = "Batch Report for Missing Letter";
             this.missingradioButton.UseVisualStyleBackColor = true;
+            this.missingradioButton.Visible = false;
             this.missingradioButton.CheckedChanged += new System.EventHandler(this.missingradioButton_CheckedChanged);
             this.missingradioButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.missingradioButton_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(67, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 60);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Successful Letters";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(326, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 60);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Unsuccessful Letters";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(563, 115);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(177, 60);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Missing Letters";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // ReportByBatch
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(544, 284);
+            this.ClientSize = new System.Drawing.Size(784, 345);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.missingradioButton);
             this.Controls.Add(this.LandscapeRadioButton);
             this.Controls.Add(this.BatchReportforSuccessfulLetter);
@@ -138,5 +183,8 @@
         private System.Windows.Forms.RadioButton BatchReportforSuccessfulLetter;
         private System.Windows.Forms.RadioButton LandscapeRadioButton;
         private System.Windows.Forms.RadioButton missingradioButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- PhonebookApp/Reports/ReportByBatch.cs
+++ PhonebookApp/Reports/ReportByBatch.cs
@@ -268,5 +268,95 @@ namespace PhonebookApp.Reports
         {
 
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ReportViewer f2 = new ReportViewer();
+            TableLogOnInfos reportLogonInfos = new TableLogOnInfos();
+            TableLogOnInfo reportLogonInfo = new TableLogOnInfo();
+            ConnectionInfo reportConInfo = new ConnectionInfo();
+            Tables tables = default(Tables);
+            //	Table table = default(Table);
+            var with1 = reportConInfo;
+            with1.ServerName = "tcp:KyotoServer,49172";
+            with1.DatabaseName = "PhoneBookDBKD22";
+            with1.UserID = "sa";
+            with1.Password = "SystemAdministrator";
+            BatchReportNoPODNum cr = new BatchReportNoPODNum();
+            tables = cr.Database.Tables;
+            foreach (Table table in tables)
+            {
+                reportLogonInfo = table.LogOnInfo;
+                reportLogonInfo.ConnectionInfo = reportConInfo;
+                table.ApplyLogOnInfo(reportLogonInfo);
+            }
+            //f2.crystalReportViewer1.ParameterFieldInfo = paramFields;
+            //set the parameterfield information in the crystal report
+            f2.crystalReportViewer1.ReportSource = cr;
+            this.Visible = false;
+
+            f2.ShowDialog();
+            this.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ReportViewer f2 = new ReportViewer();
+            TableLogOnInfos reportLogonInfos = new TableLogOnInfos();
+            TableLogOnInfo reportLogonInfo = new TableLogOnInfo();
+            ConnectionInfo reportConInfo = new ConnectionInfo();
+            Tables tables = default(Tables);
+            //	Table table = default(Table);
+            var with1 = reportConInfo;
+            with1.ServerName = "tcp:KyotoServer,49172";
+            with1.DatabaseName = "PhoneBookDBKD22";
+            with1.UserID = "sa";
+            with1.Password = "SystemAdministrator";
+            BatchReportUnSuccessfull cr = new BatchReportUnSuccessfull();
+            tables = cr.Database.Tables;
+            foreach (Table table in tables)
+            {
+                reportLogonInfo = table.LogOnInfo;
+                reportLogonInfo.ConnectionInfo = reportConInfo;
+                table.ApplyLogOnInfo(reportLogonInfo);
+            }
+            //f2.crystalReportViewer1.ParameterFieldInfo = paramFields;
+            //set the parameterfield information in the crystal report
+            f2.crystalReportViewer1.ReportSource = cr;
+            this.Visible = false;
+
+            f2.ShowDialog();
+            this.Visible = true;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            ReportViewer f2 = new ReportViewer();
+            TableLogOnInfos reportLogonInfos = new TableLogOnInfos();
+            TableLogOnInfo reportLogonInfo = new TableLogOnInfo();
+            ConnectionInfo reportConInfo = new ConnectionInfo();
+            Tables tables = default(Tables);
+            //	Table table = default(Table);
+            var with1 = reportConInfo;
+            with1.ServerName = "tcp:KyotoServer,49172";
+            with1.DatabaseName = "PhoneBookDBKD22";
+            with1.UserID = "sa";
+            with1.Password = "SystemAdministrator";
+            BatchReportL cr = new BatchReportL();
+            tables = cr.Database.Tables;
+            foreach (Table table in tables)
+            {
+                reportLogonInfo = table.LogOnInfo;
+                reportLogonInfo.ConnectionInfo = reportConInfo;
+                table.ApplyLogOnInfo(reportLogonInfo);
+            }
+            //f2.crystalReportViewer1.ParameterFieldInfo = paramFields;
+            //set the parameterfield information in the crystal report
+            f2.crystalReportViewer1.ReportSource = cr;
+            this.Visible = false;
+
+            f2.ShowDialog();
+            this.Visible = true;
+        }
     }
 }

303b234b06d89e1b50691c6bd62dc4cf5a071f11
Fariha Hossain <farihafiha@gmail.com>
2017-12-23 16:57:52 +0600

    ReportUI.Designer.cs

--- PhonebookApp/UI/ReportUI.Designer.cs
+++ PhonebookApp/UI/ReportUI.Designer.cs
@@ -185,7 +185,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(161, 65);
             this.button2.TabIndex = 7;
-            this.button2.Text = "Courier Batch";
+            this.button2.Text = "Dispatch";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
